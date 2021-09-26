25166a5d366adbbc5f3789d2eed0279d605d3b21
ChinaSnake007 <1505828047@qq.com>
2021-09-26 15:46:44 +0800

    组合问题

--- 39.cpp
+++ 39.cpp
@@ -13,55 +13,79 @@ void bianli(vector<int> a){
     cout<<endl;
 }
 
+// class Solution {
+// private:
+//     vector<vector<int>> result;
+//     vector<int> path;
+//     void backtracking(vector<int>& candidates, int target, int sum, int startIndex) {
+//         if (sum == target) {                    //终止条件
+//             result.push_back(path);
+//             return;
+//         }
+
+//         // 如果 sum + candidates[i] > target 就终止遍历
+//         for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++) {
+//             sum += candidates[i];
+//             path.push_back(candidates[i]);
+//             backtracking(candidates, target, sum, i);
+//             sum -= candidates[i];
+//             path.pop_back();
+
+//         }
+//     }
+// public:
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         result.clear();
+//         path.clear();
+//         sort(candidates.begin(), candidates.end()); // 需要排序
+//         backtracking(candidates, target, 0, 0);
+//         return result;
+//     }
+// };
+
+// // 作者：carlsun-2
+// // 链接：https://leetcode-cn.com/problems/combination-sum/solution/dai-ma-sui-xiang-lu-dai-ni-xue-tou-hui-s-7tum/
+// // 来源：力扣（LeetCode）
+// // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+// {
+//     vector<vector<int>> ans;
+//     for(int i=0;i<3;i++){
+//         vector<int> a;
+//         a.resize(i+1,i);
+//         ans.push_back(a);
+//     }
+//     return ans;
+// }
 class Solution {
-private:
-    vector<vector<int>> result;
-    vector<int> path;
-    void backtracking(vector<int>& candidates, int target, int sum, int startIndex) {
-        if (sum == target) {                    //终止条件
-            result.push_back(path);
+public:
+    vector<vector<int>> ans;
+    vector<int> a;
+    void backtracking(vector<int>candidates,int sum,int start,int target){
+        if(sum == target){
+            ans.push_back(a);
             return;
         }
-
-        // 如果 sum + candidates[i] > target 就终止遍历
-        for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++) {
-            sum += candidates[i];
-            path.push_back(candidates[i]);
-            backtracking(candidates, target, sum, i);
-            sum -= candidates[i];
-            path.pop_back();
-
+        for(int i = start;i < candidates.size()&&(sum+candidates[i]<=target);i++)
+        {
+            a.push_back(candidates[i]);
+            sum = sum + candidates[i];
+            backtracking(candidates,sum,i,target);
+            a.pop_back();
+            sum = sum - candidates[i];
         }
     }
-public:
+
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        result.clear();
-        path.clear();
-        sort(candidates.begin(), candidates.end()); // 需要排序
-        backtracking(candidates, target, 0, 0);
-        return result;
+         sort(candidates.begin(),candidates.end());
+         backtracking(candidates,0,0,target);    
+         return ans;
     }
 };
-
-// 作者：carlsun-2
-// 链接：https://leetcode-cn.com/problems/combination-sum/solution/dai-ma-sui-xiang-lu-dai-ni-xue-tou-hui-s-7tum/
-// 来源：力扣（LeetCode）
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-vector<vector<int>> combinationSum(vector<int> &candidates, int target)
-{
-    vector<vector<int>> ans;
-    for(int i=0;i<3;i++){
-        vector<int> a;
-        a.resize(i+1,i);
-        ans.push_back(a);
-    }
-    return ans;
-}
-
 int main()
 {
     vector<int> candidates = {2,3,6,7};
-    sort(candidates.begin(),candidates.end());
     int target = 7;
     Solution solution;
     vector<vector<int>> ans = solution.combinationSum(candidates, target);
