8b7bde229d9cb844ee84308f0031e05e1c62423d
ChinaSnake007 <1505828047@qq.com>
2021-09-28 19:06:27 +0800

    不同的路径

---
+++ 62.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<stack>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    
+
+
+    return 0;
+}
--- check.cpp
+++ check.cpp
@@ -2,12 +2,20 @@
 #include<vector>
 #include<map>
 #include<string>
-
+#include<map>
+#include<algorithm>
 using namespace std;
-
+map<int,string> Hash;
 
 int main(){
-    string a="123456";
-    cout<<a<<endl;
+   vector<int> a;
+   a.push_back(1);
+
+   cout<<a.back()<<endl;
+// if(iter != Hash.end())
+//        cout<<"Find, the value is"<<iter->second<<endl;
+// else
+//    cout<<"Do not Find"<<endl;
+
     return 0;
 }
