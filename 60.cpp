9f838a855ed6fd630399187dbf8721ee95a0187f
ChinaSnake007 <1505828047@qq.com>
2021-10-04 19:35:18 +0800

    排列序列

---
+++ 60.cpp
@@ -0,0 +1,24 @@
+//≈≈¡––Ú¡–
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<stack>
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+
+    }
+};
+
+using namespace std;
+typedef long long ll;
+
+int main(){
+    Solution s;
+    
+    cout<<s.getPermutation(3,3)<<endl;
+
+    return 0;
+}
